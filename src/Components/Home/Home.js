e9bf564464a71726f33115a499f044fcf1534e6d
Elizaveta Neumoina <elizabneu@gmail.com>
2020-12-08 22:01:12 +0300

    add collages output

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,7 +8,7 @@ import img4 from "../../assets/image/Gallery/img4.jpg";
 import img5 from "../../assets/image/Gallery/img5.jpg";
 import img6 from "../../assets/image/Gallery/img6.jpg";
 import logo from "../../assets/image/logo.svg"
-import {connect} from "react-redux";
+import {getCollageAll} from "../../API/CollageAPI";
 
 
 const Gallery = [
@@ -32,33 +32,47 @@ const Gallery = [
     },
 ]
 
-const Home = (props) => {
-    return (
-        <div>
-            <Header/>
-            <div className={c.main_frame}>
-                <div className={c.main_text}>
-                    <p className={c.main_text1}>
-                        Вдохновение
-                    </p>
-                    <p className={c.main_text2}>
-                        · лучшее на этой неделе ·
+class Home extends React.Component {
+    state = {
+        Photos: [],
+        count: 0,
+    }
+
+    async componentDidMount() {
+        let data = await getCollageAll("", "", "", "Likes", 0, 6)
+        this.setState({Photos: data.rows, count: data.count})
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className={c.main_frame}>
+                    <div className={c.main_text}>
+                        <p className={c.main_text1}>
+                            Вдохновение
+                        </p>
+                        <p className={c.main_text2}>
+                            · лучшие образы ·
+                        </p>
+                    </div>
+                    <div className={c.gallery}>
+                        {this.state.Photos.map(u => <img
+                            key={u.idCollage}
+                            className={c.gallery_img}
+                            src={"data:image/png;base64," + u.Photo}
+                            alt=""/>)}
+                    </div>
+                </div>
+                <div className={c.bottom}>
+                    <img className={c.icon} src={logo}/>
+                    <p className={c.quote}>
+                        Одежда украшает человека
                     </p>
                 </div>
-                <div className={c.gallery}>
-                    {
-                        Gallery.map(u => <img className={c.gallery_img} src={u.photo} alt="" key={u.photo}/>)
-                    }
-                </div>
             </div>
-            <div className={c.bottom}>
-                <img className={c.icon} src={logo}/>
-                <p className={c.quote}>
-                    Одежда украшает человека
-                </p>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Home;
