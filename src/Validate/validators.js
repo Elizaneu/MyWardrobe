e89dba4ea72ec27b50e6a6d8ee5a76fc6a47ec68
Elizaveta Neumoina <elizabneu@gmail.com>
2020-12-10 00:11:55 +0300

    add validator to check spaces

--- src/Validate/validators.js
+++ src/Validate/validators.js
@@ -18,3 +18,11 @@ export const require = data => {
     if (data && data.length > 0 && data.trim() !== '') return undefined
     return "Поле не может быть пустым"
 }
+
+export const SpaceCheck = data => {
+    if(!data) return undefined
+    for (let i = 0; i < data.length; i++){
+        if (data.charAt(i) ===" ") return "Поле не может содержать пробелы"
+    }
+    return undefined
+}
